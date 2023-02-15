83b6102ea349196b8293b2866cb18281bdf137e1
Tom Damen <twjdamen@gmail.com>
2023-02-15 16:04:13 +0100

    make room files

--- database/migrations/2023_02_15_154459_create_rooms_table.php
+++ database/migrations/2023_02_15_154459_create_rooms_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
+            $table->integer('capacity');
+            $table->tinyText('bed_configuration');
+            $table->tinyText('room_number');
+            $table->float('base_price_per_night');
+            $table->foreignId('cleaning_status_id');
+            $table->boolean('baby_bed_possible');
+            $table->foreignId('window_view_id');
+            $table->foreignId('room_type_id');
+            $table->mediumText('description');
+            $table->mediumText('status_comment');
             $table->timestamps();
         });
     }
