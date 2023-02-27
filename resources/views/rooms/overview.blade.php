4a31d3d8004f66ad8c55cc13676341b59d51347a
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-02-27 14:27:41 +0100

    html

---
+++ resources/views/rooms/overview.blade.php
@@ -0,0 +1,15 @@
+<main id="rooms-overview">
+    <div class="left-side">
+        <div class="filter">
+
+        </div>
+        <div class="rooms-container">
+            <div class="room-item">
+
+            </div>
+        </div>
+    </div>
+    <div class="right-side">
+
+    </div>
+</main>
