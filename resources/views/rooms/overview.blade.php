f465f5cb5d6a5ed29dc56216079dfac341366efc
rogerfate <rogersnelders2001@gmail.com>
2023-03-01 15:43:46 +0100

    Make scss(no buttons)

--- resources/views/rooms/overview.blade.php
+++ resources/views/rooms/overview.blade.php
@@ -1,15 +1,35 @@
+<x-layout.base/>
 <main id="rooms-overview">
     <div class="left-side">
         <div class="filter">
 
         </div>
+        <h2>Rooms</h2>
         <div class="rooms-container">
+            <div class="room-item">
+                <div>
+                    <h3>Room nr.</h3>
+                    <h3>Price</h3>
+                </div>
+                <p>room type</p></br>
+                <p>available</p>
+            </div>
+            <div class="room-item">
+                
+            </div>
             <div class="room-item">
                 
             </div>
         </div>
     </div>
     <div class="right-side">
-
+        <h2>Details</h2>
+        <p>Number</p>
+        <p>Size</p>
+        <p>Availability</p>
+        <p>Cleaning status</p>
+        <p>Bed configuration</p>
+        <p>View</p>
+        <p>Comments</p>
     </div>
 </main>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('rooms/overview');
 });
 
 Route::get('/dashboard', function () {
