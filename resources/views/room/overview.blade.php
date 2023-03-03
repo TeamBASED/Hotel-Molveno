5a06949eb76516adf4747511144c47691780102d
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-03-03 12:28:39 +0100

    make room data dynamic

--- resources/views/room/overview.blade.php
+++ resources/views/room/overview.blade.php
@@ -6,15 +6,21 @@
         </div>
         <h2>Rooms</h2>
         <div class="rooms-container">
-            <div class="room-item">
-                <div>
-                    <h3>Room nr.</h3>
-                    <h3>Price</h3>
+
+            @foreach ($rooms as $room)
+            
+                <div class="room-item">
+                    <div>
+                        <h3>Nr. {{ $room->room_number }}</h3>
+                        <h3>{{ $room->base_price_per_night }} per night</h3>
+                    </div>
+                    <p>{{ $room->roomType->type }}</p>
+                    <p class="margin-top">available</p>
+                    <x-buttons.primary-button>Make reservation</x-buttons.primary-button>
                 </div>
-                <p>room type</p>
-                <p class="margin-top">available</p>
-                <x-buttons.primary-button>Make reservation</x-buttons.primary-button>
-            </div>
+                
+            @endforeach
+                
         </div>
     </div>
     <div class="right-side">
