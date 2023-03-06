147e88a65234ec22158db32a0a46bbd8a9d436ea
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-03-06 12:15:17 +0100

    make component for room card in overview

--- resources/views/room/overview.blade.php
+++ resources/views/room/overview.blade.php
@@ -8,16 +8,8 @@
             <div class="rooms-container">
 
                 @foreach ($rooms as $room)
-                
-                    <div class="room-item">
-                        <div>
-                            <h3>Nr. {{ $room->room_number }}</h3>
-                            <h3>{{ $room->base_price_per_night }} per night</h3>
-                        </div>
-                        <p>{{ $room->roomType->type }}</p>
-                        <p class="margin-top">available</p>
-                        <x-buttons.primary-button :href="route('room.info', ['id' => $room->id])" >Go to info</x-buttons.primary-button>
-                    </div>
+
+                    <x-room.card :room="$room" />
                     
                 @endforeach
                     
