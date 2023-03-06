debdd6b74bb95b4919b4c86186498348b8d22bf3
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-03-06 10:44:04 +0100

    change room button to link to info

--- resources/views/room/overview.blade.php
+++ resources/views/room/overview.blade.php
@@ -1,40 +1,41 @@
-<x-layout.base/>
-<main id="room-overview">
-    <div class="left-side">
-        <div class="filter">
+<x-layout.base>
+    <main id="room-overview">
+        <div class="left-side">
+            <div class="filter">
 
-        </div>
-        <h2>Rooms</h2>
-        <div class="rooms-container">
+            </div>
+            <h2>Rooms</h2>
+            <div class="rooms-container">
 
-            @foreach ($rooms as $room)
-            
-                <div class="room-item">
-                    <div>
-                        <h3>Nr. {{ $room->room_number }}</h3>
-                        <h3>{{ $room->base_price_per_night }} per night</h3>
+                @foreach ($rooms as $room)
+                
+                    <div class="room-item">
+                        <div>
+                            <h3>Nr. {{ $room->room_number }}</h3>
+                            <h3>{{ $room->base_price_per_night }} per night</h3>
+                        </div>
+                        <p>{{ $room->roomType->type }}</p>
+                        <p class="margin-top">available</p>
+                        <x-buttons.primary-button :href="route('room.info', ['id' => $room->id])" >Go to info</x-buttons.primary-button>
                     </div>
-                    <p>{{ $room->roomType->type }}</p>
-                    <p class="margin-top">available</p>
-                    <x-buttons.primary-button>Make reservation</x-buttons.primary-button>
-                </div>
-                
-            @endforeach
-                
+                    
+                @endforeach
+                    
+            </div>
         </div>
-    </div>
-    <div class="right-side">
-        <h2>Details</h2>
-        <p>Number</p>
-        <p>Size</p>
-        <p>Availability</p>
-        <p>Cleaning status</p>
-        <p>Bed configuration</p>
-        <p>View</p>
-        <p>Comments</p>
-        <div class="margin-top">
-            <x-buttons.secondary-button>Make reservation</x-buttons.secondary-button>
-            <x-buttons.secondary-button>Open room info</x-buttons.secondary-button>
+        <div class="right-side">
+            <h2>Details</h2>
+            <p>Number</p>
+            <p>Size</p>
+            <p>Availability</p>
+            <p>Cleaning status</p>
+            <p>Bed configuration</p>
+            <p>View</p>
+            <p>Comments</p>
+            <div class="margin-top">
+                <x-buttons.secondary-button>Make reservation</x-buttons.secondary-button>
+                <x-buttons.secondary-button>Open room info</x-buttons.secondary-button>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
+</x-layout.base> 
