285e26ccb1a3b3c8c890f6de67d8136084cef68e
Tom Damen <twjdamen@gmail.com>
2023-03-06 16:06:53 +0100

    made room edit page

--- resources/views/room/edit.blade.php
+++ resources/views/room/edit.blade.php
@@ -2,30 +2,45 @@
 <main id="room-edit">
 
     <div class="container-edit">
-        <div class="delete-button-container">
+        <div class="three-column-mid-stretch">
+            <div class="blank"></div>
             <h1>Edit room</h1>
-            <form action=""><x-buttons.primary-button class="button gray-bg">Delete</x-buttons.primary-button></form>
-        </div>
-            <form action="">
-            @csrf
-                <div class="left">
-                    <input type="text" class="input-text" placeholder="Room number">
-                    <input type="text" class="input-text" placeholder="Size">
-                    <select name="Room view" class="dropdown-select">
-                            <option value="">Test</option>
-                    </select>   
-                    <input type="text" class="input-text" placeholder="Price per night">
-                    <!-- view = dropdown -->
+            <form action="" class="delete-form"><x-buttons.primary-button class="button gray-bg right-aligned">Delete</x-buttons.primary-button></form>
+        <div class="blank"></div>
+        <form action="" class="edit-room-form">
+        @csrf
+            <div class="left">
+                <input type="text" class="input-text" placeholder="Room number" name="room-number">
+                <input type="text" class="input-text" placeholder="Capacity" name="capacity">
+                <input type="text" class="input-text" placeholder="Price per night" name="price-per-night">
+                <input type="text" class="input-text" placeholder="Bed configuration" name="bed-configuration">
+                <!-- view = dropdown -->
+            </div>
+            <div class="middle">
+                <div class="babybed">
+                    <label for="baby-bed">Baby bed possible:</label>
+                    <input type="checkbox" class="input-text" name="baby-bed">
                 </div>
-                <div class="right">
-                    <input type="text" class="input-text" placeholder="Bed configuration">
-                    <input type="text" class="input-text" id="room-edit-description" placeholder="Room description">
-                </div>
-            </form>
+                <input type="text" class="input-text" id="room-edit-description" placeholder="Room description" name="room-edit-description">
+            </div>
+            <div class="right">
+                <select name="Room view" class="dropdown-select" name="room-view">
+                        <option value="">Test</option>
+                </select>
+                <select name="Size" class="dropdown-select" name="size">
+                        <option value="">Test</option>
+                </select>
+            </div>
+        </form>
+        <div class="blank"></div>
+            
+
     </div>
- 
-    <div class="buttons">
-        <x-buttons.primary-button class="button gray-bg">Cancel</x-buttons.primary-button>
+    <div class="flex-space-between buttons">
+                <x-buttons.primary-button class="button gray-bg">Cancel</x-buttons.primary-button>
         <x-buttons.primary-button class="button">Save</x-buttons.primary-button>
     </div>
+    </div>
+ 
+
 </main>
