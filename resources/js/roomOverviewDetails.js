dc2bcd387402b37b14099e21b9b8139b268e455c
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-03-08 12:45:43 +0100

    make room info link and hide details on load

--- resources/js/roomOverviewDetails.js
+++ resources/js/roomOverviewDetails.js
@@ -4,6 +4,8 @@
 
 const roomsContainer = document.querySelector('#rooms-container');
 
+const detailsSection = document.querySelector('#details-section');
+
 const detailsNumber = document.querySelector('#details-number');
 const detailsType = document.querySelector('#details-type');
 const detailsPricePerNight = document.querySelector('#details-price-per-night');
@@ -12,7 +14,8 @@ const detailsBedConfiguration = document.querySelector('#details-bed-configurati
 const detailsView = document.querySelector('#details-view');
 const detailsDescription = document.querySelector('#details-description');
 const detailsCleaningStatus = document.querySelector('#details-cleaning-status');
-const detailsStatusComment = document.querySelector('#details-status-comment');
+
+const detailsInfoButton = document.querySelector('#details-info-button');
 
 // Methods
 
@@ -21,17 +24,26 @@ roomsContainer.addEventListener('click', e => {
 
     if(target.closest('.room-item')) {
         handleRoomSelect(target.closest('.room-item'));
+
+        if(detailsSection.matches('.hidden')) {
+            showDetailsSection();
+        }
     }
 })
+
+function showDetailsSection() {
+    detailsSection.classList.remove('hidden');
+}
 
 function handleRoomSelect(room) {
     const roomDetails = JSON.parse(room.dataset.roomDetails);
     const roomType = room.dataset.roomType;
     const roomView = room.dataset.roomView;
     const cleaningStatus = room.dataset.cleaningStatus;
-    console.log(roomDetails);
     
     setRoomDetails(roomDetails, roomType, roomView, cleaningStatus);
+
+    detailsInfoButton.pathname = getPathName(roomDetails.id);
 }
 
 function setRoomDetails(roomDetails, roomType, roomView, cleaningStatus) {
@@ -43,9 +55,12 @@ function setRoomDetails(roomDetails, roomType, roomView, cleaningStatus) {
     setElementText(detailsView, roomView);
     setElementText(detailsDescription, roomDetails.description);
     setElementText(detailsCleaningStatus, cleaningStatus);
-    setElementText(detailsStatusComment, roomDetails.status_comment);
 }
 
 function setElementText(element, text) {
     element.innerText = text;
+}
+
+function getPathName(id) {
+    return `/room/${id}/info`;
 }
--- resources/views/room/overview.blade.php
+++ resources/views/room/overview.blade.php
@@ -20,7 +20,7 @@
                     
             </div>
         </div>
-        <div class="details-section">
+        <div id="details-section" class="hidden">
             <h2>Details</h2>
 
             <p class="details-label">Number</p>
@@ -29,17 +29,17 @@
             <p class="details-label">Type</p>
             <p id="details-type"></p>
 
-            <p class="details-label">Price per night</p>
-            <p id="details-price-per-night"></p>
-
             <p class="details-label">Capacity</p>
             <p id="details-capacity"></p>
 
-            <p class="details-label">Bed configuration</p>
-            <p id="details-bed-configuration"></p>
+            <p class="details-label">Price per night</p>
+            <p id="details-price-per-night"></p>
 
             <p class="details-label">View</p>
             <p id="details-view"></p>
+
+            <p class="details-label">Bed configuration</p>
+            <p id="details-bed-configuration"></p>
 
             <p class="details-label">Description</p>
             <p id="details-description"></p>
@@ -47,13 +47,12 @@
             <p class="details-label">Cleaning status</p>
             <p id="details-cleaning-status"></p>
 
-            <p class="details-label">Comments</p>
-            <p id="details-status-comment"></p>
-
             <div class="margin-top">
                 <!-- <x-buttons.secondary-button id="details-reservation-button">Make reservation</x-buttons.secondary-button> -->
-                <x-buttons.secondary-button id="details-info-button">Open room info</x-buttons.secondary-button>
+                <x-buttons.secondary-button id="details-info-button" :href="route('room.info', ['id' => 1])">Open room info</x-buttons.secondary-button>
             </div>
+
+            <p id="select-room-message">Click a room to view details.</p>
         </div>
     </main>
 </x-layout.base> 
