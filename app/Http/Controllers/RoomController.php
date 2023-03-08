87ea85cd5b01cdc80108853b3f5958857512a6ec
Sinyara <87365280+Sinyara@users.noreply.github.com>
2023-03-08 10:57:50 +0100

    Change / to . in roomcontroller

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -11,27 +11,27 @@ class RoomController extends Controller
     public function viewRoomInfo(int $id){
         $room = Room::getRoomData($id);
 
-        return view('/room/info', ['room' => $room]);
+        return view('room.info', ['room' => $room]);
     }
 
     public function viewRoomOverview() {
         $rooms = Room::getAllRoomData();
 
-        return view('/room/overview', ['rooms' => $rooms]);
+        return view('room.overview', ['rooms' => $rooms]);
     }
 
     public function viewRoomEdit() {
         $roomTypes = RoomType::pluck("type");
         $roomViews = RoomView::pluck("type");
         
-        return view('/room/edit', ['roomTypes' => $roomTypes, 'roomViews' => $roomViews]);
+        return view('room.edit', ['roomTypes' => $roomTypes, 'roomViews' => $roomViews]);
     }
 
     public function viewRoomCreate() {
         $roomTypes = RoomType::pluck("type");
         $roomViews = RoomView::pluck("type");
         
-        return view('/room/create', ['roomTypes' => $roomTypes, 'roomViews' => $roomViews]);
+        return view('room.create', ['roomTypes' => $roomTypes, 'roomViews' => $roomViews]);
     }
 
 
