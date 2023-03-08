2986b330b45a5021fd15e5248bfd1c56fa334996
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-03-08 10:17:23 +0100

    add ids to elements

---
+++ resources/js/roomOverviewDetails.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Declarations
+
+const detailsNumber = document.querySelector('#details-number');
+const detailsType = document.querySelector('#details-type');
+const detailsPricePerNight = document.querySelector('#details-price-per-night');
+const detailsCapacity = document.querySelector('#details-capacity');
+const detailsBedConfiguration = document.querySelector('#details-bed-configuration');
+const detailsView = document.querySelector('#details-view');
+const detailsDescription = document.querySelector('#details-description');
+const detailsCleaningStatus = document.querySelector('#details-cleaning-status');
+const detailsStatusComment = document.querySelector('#details-comments');
+
+// Methods
+
--- resources/views/room/overview.blade.php
+++ resources/views/room/overview.blade.php
@@ -5,7 +5,7 @@
 
             </div>
             <h2>Rooms</h2>
-            <div class="rooms-container">
+            <div id="rooms-container">
 
                 @foreach ($rooms as $room)
 
@@ -17,16 +17,19 @@
         </div>
         <div class="right-side">
             <h2>Details</h2>
-            <p>Number</p>
-            <p>Size</p>
-            <p>Availability</p>
-            <p>Cleaning status</p>
-            <p>Bed configuration</p>
-            <p>View</p>
-            <p>Comments</p>
+            <p id="details-number">Number</p>
+            <p id="details-type">Type</p>
+            <p id="details-price-per-night">Price per night</p>
+            <p id="details-capacity">Capacity</p>
+            <p id="details-bed-configuration">Bed configuration</p>
+            <p id="details-view">View</p>
+            <p id="details-description">Description</p>
+            <p id="details-cleaning-status">Cleaning status</p>
+            <p id="details-status-comment">Comments</p>
+
             <div class="margin-top">
-                <x-buttons.secondary-button>Make reservation</x-buttons.secondary-button>
-                <x-buttons.secondary-button>Open room info</x-buttons.secondary-button>
+                <!-- <x-buttons.secondary-button id="details-reservation-button">Make reservation</x-buttons.secondary-button> -->
+                <x-buttons.secondary-button id="details-info-button">Open room info</x-buttons.secondary-button>
             </div>
         </div>
     </main>
