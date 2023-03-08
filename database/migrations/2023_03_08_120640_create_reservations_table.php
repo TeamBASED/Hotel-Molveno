c85df556ec222b0f5129b93ca892e11f5bb91abd
Tom Damen <twjdamen@gmail.com>
2023-03-08 12:30:40 +0100

    make migrations for user roles

---
+++ database/migrations/2023_03_08_120640_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->int('contact_id');
+            $table->date('date_of_arrival');
+            $table->date('date_of_departure');
+            $table->int('bill_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
---
+++ database/migrations/2023_03_08_121337_create_guest_reservations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guest_reservations', function (Blueprint $table) {
+            $table->id();
+            $table->int('reservation_id');
+            $table->int('guest_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guest_reservations');
+    }
+};
