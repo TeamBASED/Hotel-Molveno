f3e753dd8fce52d988678aad4509c1fb2b4c2bbb
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-03-08 11:13:14 +0100

    make details display functionality

--- resources/js/roomOverviewDetails.js
+++ resources/js/roomOverviewDetails.js
@@ -2,6 +2,8 @@
 
 // Declarations
 
+const roomsContainer = document.querySelector('#rooms-container');
+
 const detailsNumber = document.querySelector('#details-number');
 const detailsType = document.querySelector('#details-type');
 const detailsPricePerNight = document.querySelector('#details-price-per-night');
@@ -10,7 +12,40 @@ const detailsBedConfiguration = document.querySelector('#details-bed-configurati
 const detailsView = document.querySelector('#details-view');
 const detailsDescription = document.querySelector('#details-description');
 const detailsCleaningStatus = document.querySelector('#details-cleaning-status');
-const detailsStatusComment = document.querySelector('#details-comments');
+const detailsStatusComment = document.querySelector('#details-status-comment');
 
 // Methods
 
+roomsContainer.addEventListener('click', e => {
+    const target = e.target;
+
+    if(target.closest('.room-item')) {
+        handleRoomSelect(target.closest('.room-item'));
+    }
+})
+
+function handleRoomSelect(room) {
+    const roomDetails = JSON.parse(room.dataset.roomDetails);
+    const roomType = room.dataset.roomType;
+    const roomView = room.dataset.roomView;
+    const cleaningStatus = room.dataset.cleaningStatus;
+    console.log(roomDetails);
+    
+    setRoomDetails(roomDetails, roomType, roomView, cleaningStatus);
+}
+
+function setRoomDetails(roomDetails, roomType, roomView, cleaningStatus) {
+    setElementText(detailsNumber, roomDetails.room_number);
+    setElementText(detailsType, roomType);
+    setElementText(detailsPricePerNight, roomDetails.base_price_per_night);
+    setElementText(detailsCapacity, roomDetails.capacity);
+    setElementText(detailsBedConfiguration, roomDetails.bed_configuration);
+    setElementText(detailsView, roomView);
+    setElementText(detailsDescription, roomDetails.description);
+    setElementText(detailsCleaningStatus, cleaningStatus);
+    setElementText(detailsStatusComment, roomDetails.status_comment);
+}
+
+function setElementText(element, text) {
+    element.innerText = text;
+}
--- resources/views/room/overview.blade.php
+++ resources/views/room/overview.blade.php
@@ -1,4 +1,9 @@
 <x-layout.base>
+
+    <x-slot:resources>
+        @vite('resources/js/roomOverviewDetails.js')
+    </x-slot>
+
     <main id="room-overview">
         <div class="left-side">
             <div class="filter">
@@ -15,17 +20,35 @@
                     
             </div>
         </div>
-        <div class="right-side">
+        <div class="details-section">
             <h2>Details</h2>
-            <p id="details-number">Number</p>
-            <p id="details-type">Type</p>
-            <p id="details-price-per-night">Price per night</p>
-            <p id="details-capacity">Capacity</p>
-            <p id="details-bed-configuration">Bed configuration</p>
-            <p id="details-view">View</p>
-            <p id="details-description">Description</p>
-            <p id="details-cleaning-status">Cleaning status</p>
-            <p id="details-status-comment">Comments</p>
+
+            <p class="details-label">Number</p>
+            <p id="details-number"></p>
+
+            <p class="details-label">Type</p>
+            <p id="details-type"></p>
+
+            <p class="details-label">Price per night</p>
+            <p id="details-price-per-night"></p>
+
+            <p class="details-label">Capacity</p>
+            <p id="details-capacity"></p>
+
+            <p class="details-label">Bed configuration</p>
+            <p id="details-bed-configuration"></p>
+
+            <p class="details-label">View</p>
+            <p id="details-view"></p>
+
+            <p class="details-label">Description</p>
+            <p id="details-description"></p>
+
+            <p class="details-label">Cleaning status</p>
+            <p id="details-cleaning-status"></p>
+
+            <p class="details-label">Comments</p>
+            <p id="details-status-comment"></p>
 
             <div class="margin-top">
                 <!-- <x-buttons.secondary-button id="details-reservation-button">Make reservation</x-buttons.secondary-button> -->
