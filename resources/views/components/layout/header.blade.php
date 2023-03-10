e160a620fe8c1ef07bcb3fbfd93354b57c6ebe0f
Tom Damen <twjdamen@gmail.com>
2023-03-10 09:54:34 +0100

    removed non working buttons and links

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -4,11 +4,11 @@
         <h1>Hotel Molveno</h1>
     </div>
     <div class="bottom">
-        <nav>
+        {{-- <nav>
             <a class="navigation-link" href="#">Home</a>
             <a class="navigation-link" href="#">Room</a>
             <a class="navigation-link" href="#">Reservations</a>
         </nav>
-        <a class="logout-link" href="#">Logout</a>
+        <a class="logout-link" href="#">Logout</a> --}}
     </div>
 </header>
--- resources/views/room/info.blade.php
+++ resources/views/room/info.blade.php
@@ -19,7 +19,7 @@
                     <div class="info-container"><h4>Description&colon;</h4><p> {{ $room->description }}</p></div>
                 </div>
                 <div class="button-container">
-                    <x-buttons.primary-button>Edit</x-buttons.primary-button>
+                    {{-- <x-buttons.primary-button>Edit</x-buttons.primary-button> --}}
                 </div>
             </section>
             <section class="reservation-schedule">
