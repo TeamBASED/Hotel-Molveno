eea8b55482404319fc32feeae17d39e143c21946
Sinyara <87365280+Sinyara@users.noreply.github.com>
2023-03-17 10:13:55 +0100

    Delete e-mail related tables in user-table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,6 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->integer('role_id');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
