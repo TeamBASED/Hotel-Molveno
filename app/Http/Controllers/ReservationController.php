44bd9af4d83a0f8a3f4529d012652713ece6211f
Sinyara <87365280+Sinyara@users.noreply.github.com>
2023-03-20 11:42:40 +0100

    Not working

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function viewReservationOverview() {
+        $rooms = Room::getAllRoomData();
+
+        return view('reservation.overview', ['rooms' => $rooms]);
+    }
 }
--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+    public static function getAllReservationData() {
+        return Reservation::get();
+    }
 }
