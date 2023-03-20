40012fff4e0b998f10092b8e15965358cc46fb69
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-03-20 11:55:13 +0100

    make login and change redirects

--- routes/web.php
+++ routes/web.php
@@ -16,30 +16,30 @@ use App\Http\Controllers\RoomController;
 |
 */
 
-// Breeze stuff
-
+// Breeze dashboard, will be replaced by user management pages
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// User logged in
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-// for now redirect, this should be home page
-Route::get('/', function () {
-    return redirect(route('room.overview'));
-});
+    // for now redirect, this should be home page
+    Route::get('/', function () {
+        return redirect(route('room.overview'));
+    });
 
-// Room routes
-Route::get('/room/overview', [RoomController::class, 'viewRoomOverview'])->name('room.overview');
-Route::get('/room/{id}/info', [RoomController::class, 'viewRoomInfo'])->name('room.info');
-Route::get('/room/create', [RoomController::class, 'viewRoomCreate'])->name('room.create');
-Route::post('/room/store', [RoomController::class, 'handleCreateRoom'])->name('room.store');
-Route::get('/room/{id}/edit', [RoomController::class, 'viewRoomEdit'])->name('room.edit');
-Route::patch('/room/{id}/update', [RoomController::class, 'handleUpdateRoom'])->name('room.update');
-Route::delete('/room/{id}/delete', [RoomController::class, 'handleDeleteRoom'])->name('room.delete');
+    // Room routes
+    Route::get('/room/overview', [RoomController::class, 'viewRoomOverview'])->name('room.overview');
+    Route::get('/room/{id}/info', [RoomController::class, 'viewRoomInfo'])->name('room.info');
+    Route::get('/room/create', [RoomController::class, 'viewRoomCreate'])->name('room.create');
+    Route::post('/room/store', [RoomController::class, 'handleCreateRoom'])->name('room.store');
+    Route::get('/room/{id}/edit', [RoomController::class, 'viewRoomEdit'])->name('room.edit');
+    Route::patch('/room/{id}/update', [RoomController::class, 'handleUpdateRoom'])->name('room.update');
+    Route::delete('/room/{id}/delete', [RoomController::class, 'handleDeleteRoom'])->name('room.delete');
+});
 
 require __DIR__.'/auth.php';
