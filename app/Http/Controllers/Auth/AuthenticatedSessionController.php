71c56cf357a166c010015f5005cd51588b4eabf1
Sinyara <87365280+Sinyara@users.noreply.github.com>
2023-03-20 10:50:09 +0100

    Add login functionality

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -25,6 +25,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+
         $request->authenticate();
 
         $request->session()->regenerate();
