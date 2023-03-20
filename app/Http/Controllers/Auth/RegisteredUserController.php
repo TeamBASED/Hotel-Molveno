f2cb98d5895075a79de178609836bede3d6d265e
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2023-03-20 10:30:18 +0100

    do not login as registered user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -49,8 +49,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
         return redirect(RouteServiceProvider::HOME);
     }
 }
