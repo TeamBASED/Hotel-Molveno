95b2a6a158e09503201dbeb277fb040e98c2859a
rogerfate <rogersnelders2001@gmail.com>
2023-03-22 12:26:39 +0100

    made frontend

--- resources/views/reservation/info.blade.php
+++ resources/views/reservation/info.blade.php
@@ -1,47 +1,48 @@
 <x-layout.base>
 <main id="reservation-info" class="main-content">
-    <h2>Room info</h2>
+    <h2>Reservation info</h2>
     <article class="flex-space-around">
         <div class="info-wrapper">
             <section class="info-section">
                 <div class="grid-two-columns margin-block">
-                    <h4>Room no.&colon;</h4>
+                    <h4>First name&colon;</h4>
                     <p class="right-aligned"></p>
-                    <h4>Capacity&colon;</h4>
+                    <h4>Last name&colon;</h4>
                     <p class="right-aligned"></p>
-                    <h4>Price per night&colon;</h4>
+                    <h4>E-mail&colon;</h4>
                     <p class="right-aligned"></p>
-                    <h4>View&colon;</h4>
+                    <h4>Telephone&colon;</h4>
                     <p class="right-aligned"></p>
-                    <h4>Bed configuration&colon;</h4>
+                    <h4>adress&colon;</h4>
                     <p class="right-aligned"></p>
-                    <h4 class="grid-span-2">Description&colon;</h4>
-                    <p class="grid-span-2"></p>
-
+                    <h4>Date arrival&colon;</h4>
+                    <p class="right-aligned"></p>
+                    <h4>Date departure&colon;</h4>
+                    <p class="right-aligned"></p>
                 </div>
                 <x-buttons.primary-button>Edit</x-buttons.primary-button>
             </section>
-            <x-buttons.primary-button :href="route('room.overview')">Back</x-buttons.primary-button>
+            <x-buttons.primary-button>Back</x-buttons.primary-button>
         </div>
         <section class="reservation-schedule">
-            <h3 class="white-text">Reservation Schedule</h3>
+            <h3 class="white-text">being reserved by</h3>
             <div class="reservations grid-two-columns">
-            <p class="white-background">Placeholder 23-05 - 28-05</p>
+                <p class="white-background">Placeholder</p>
                 <x-buttons.edit-button>Edit</x-buttons.edit-button>
             
-                <p class="white-background">Placeholder 30-05 - 01-06</p>
+                <p class="white-background">Placeholder</p>
                 <x-buttons.edit-button>Edit</x-buttons.edit-button>
             
-                <p class="white-background">Placeholder 03-06 - 10-06</p>
+                <p class="white-background">Placeholder</p>
                 <x-buttons.edit-button>Edit</x-buttons.edit-button>
 
-                <p class="white-background">Placeholder 23-06 - 28-06</p>
+                <p class="white-background">Placeholder</p>
                 <x-buttons.edit-button>Edit</x-buttons.edit-button>
 
-                <p class="white-background">Placeholder 23-07 - 27-07</p>
+                <p class="white-background">Placeholder</p>
                 <x-buttons.edit-button>Edit</x-buttons.edit-button>
 
-                <p class="white-background">Placeholder 02-08 - 18-08</p>
+                <p class="white-background">Placeholder</p>
                 <x-buttons.edit-button>Edit</x-buttons.edit-button>
             </div>
         </section>
