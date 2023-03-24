3e5c67829fde1a32444caa78f32da9afceb0bdd3
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2023-03-24 10:38:32 +0100

    make registration page frontend

--- resources/views/reservation/create.blade.php
+++ resources/views/reservation/create.blade.php
@@ -9,25 +9,25 @@
                 </ul>
             </div>
         @endif
-        <article class="flex-space-around">
-            <div id="reservation-form" class="margin-block">
-                <h1>Create room</h1>
-                <form action="" method="POST">
-                    @csrf
-                    <div class="flex-column margin-block">
+        <h2>Create reservation</h2>
+        <div class="margin-block">
+            <form id="reservation-form" class="flex-column padding-block-1rem" action="" method="POST">
+                @csrf
+                <div class="flex-space-between">
+                    <div id="input-box" class="flex-column padding-block-1rem">
                         <input type="text" class="input-text" required placeholder="Contact Name" name="contactname">
                         <label for="arrival">Date of arrival&colon;</label>
                         <input type="date" class="input-text" required name="arrival">
                         <label for="departure">Date of departure&colon;</label>
                         <input type="date" class="input-text" required name="departure">
                     </div>
+                    <x-room.infobox :room="$room" />
+                </div>
+                <div id="button-box" class="flex-space-between">
+                    <x-buttons.primary-button :href="route('room.overview')" class="button gray-background flex-center-center">Cancel</x-buttons.primary-button>
                     <x-buttons.primary-button class="button">Save</x-buttons.primary-button>
-                </form>
-            </div>
-            <x-room.infobox :room="$room" />
-            <div class="buttons">
-                <x-buttons.primary-button :href="route('room.overview')" class="button gray-bg">Cancel</x-buttons.primary-button>
-            </div>
-        </article>
+                </div>
+            </form>
+        </div>
     </main>
 </x-layout.base>
