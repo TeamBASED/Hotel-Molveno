1d6d12ceddd177c4bb9709df36f8032852a84048
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2023-04-07 11:36:00 +0200

    make separate guest overview component

--- resources/views/guest/create.blade.php
+++ resources/views/guest/create.blade.php
@@ -44,18 +44,7 @@
                 </div>
             </form>
 
-            <div class="guestOverview">
-
-                <h3>Current guests</h3>
-
-                @foreach ($reservation->guests as $guest)
-                    <div class="flex-space-between">
-                        <p>{{ $guest->first_name }} {{ $guest->last_name }}</p>
-                        <x-buttons.edit-button />
-                    </div>
-                @endforeach
-
-            </div>
+            <x-guest.overview-box :$reservation />
 
             <x-buttons.primary-button
                 :href="route('room.overview')"
