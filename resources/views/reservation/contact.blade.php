108f6d35787f1be3921ecef090dc226588cff10f
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-04-12 11:17:58 +0200

    change layout of email check page

--- resources/views/reservation/contact.blade.php
+++ resources/views/reservation/contact.blade.php
@@ -1,10 +1,12 @@
 <x-layout.base>
-    <main id="contact-identification">
-        <p>Enter Email to verify if a contact already exists.</p>
-        <form action="{{ route('reservation.verify', ['roomId' => $roomId]) }}" method="POST">
-            @csrf
-            <input type="email" name="email" id="" placeholder="Email">
-            <x-buttons.primary-button class="button">Verify</x-buttons.primary-button>
-        </form>
+    <main id="contact-identification" class="main-content flex-center">
+        <div>
+            <p>Enter Email to verify if a contact already exists.</p>
+            <form action="{{ route('reservation.verify', ['roomId' => $roomId]) }}" method="POST">
+                @csrf
+                <input type="email" name="email" class="input-text" placeholder="Email" required>
+                <x-buttons.primary-button>Verify</x-buttons.primary-button>
+            </form>
+        </div>
     </main>
-</x-layout.base> 
+</x-layout.base>
