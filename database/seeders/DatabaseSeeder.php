773d31ceff6aa71d9aed48d2dfb4d745dde284ae
Sinyara <87365280+Sinyara@users.noreply.github.com>
2023-04-12 14:52:00 +0200

    Update userseed for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,12 +31,6 @@ class DatabaseSeeder extends Seeder
         $this->call(BedConfigurationSeeder::class);
         $this->call(RoomMaintenanceSeeder::class);
         $this->call(RoomBedConfigurationSeeder::class);
-        User::create([
-            'username' => 'TeamBased', 
-            'first_name' => 'Team',
-            'last_name' => 'Based',
-            'role_id' => 1,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
-        ]);
+        $this->call(UserSeeder::class);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +15,44 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::factory()->create([
+            'username' => 'TeamBased', 
+            'first_name' => 'Team',
+            'last_name' => 'Based',
+            'role_id' => 1,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+        ]);
+
+        User::factory()->create([
+            'username' => 'owner',
+            'first_name' => 'Idske',
+            'last_name' => 'Regtien',
+            'role_id' => '1',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+        ]);
+
+        User::factory()->create([
+            'username' => 'hotel manager',
+            'role_id' => '2',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+        ]);
+
+        User::factory()->create([
+            'username' => 'head-housekeeping',
+            'role_id' => '3',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+        ]);
+
+        User::factory()->create([
+            'username' => 'housekeeping',
+            'role_id' => '4',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+        ]);
+
+        User::factory()->create([
+            'username' => 'reception',
+            'role_id' => '5',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+        ]);
     }
 }
