ab05d13f740b64992f145434c868e3270f06900d
Sinyara <87365280+Sinyara@users.noreply.github.com>
2023-04-14 11:16:33 +0200

    Add logout

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\GuestController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,9 @@ use App\Http\Controllers\ReservationController;
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// User logout
+Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 // User logged in
 Route::middleware('auth')->group(function () {
