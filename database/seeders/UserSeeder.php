9d385e22386c2499f073b4760463a9d47aa05b01
Sinyara <87365280+Sinyara@users.noreply.github.com>
2023-04-14 10:09:00 +0200

    Password in factory, add all entryfields in factory

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,40 +19,33 @@ class UserSeeder extends Seeder
             'username' => 'TeamBased', 
             'first_name' => 'Team',
             'last_name' => 'Based',
-            'role_id' => 1,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
-        ]);
+            'role_id' => 1,        ]);
 
         User::factory()->create([
             'username' => 'owner',
             'first_name' => 'Idske',
             'last_name' => 'Regtien',
             'role_id' => '1',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
         ]);
 
         User::factory()->create([
             'username' => 'hotel manager',
             'role_id' => '2',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
         ]);
 
         User::factory()->create([
             'username' => 'head-housekeeping',
             'role_id' => '3',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
         ]);
 
         User::factory()->create([
             'username' => 'housekeeping',
             'role_id' => '4',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
         ]);
 
         User::factory()->create([
             'username' => 'reception',
             'role_id' => '5',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
         ]);
     }
 }
