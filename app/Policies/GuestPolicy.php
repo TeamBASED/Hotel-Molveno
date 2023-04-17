b88d411f06d87d96fc399225bc3c012af1fdbd40
Sinyara <87365280+Sinyara@users.noreply.github.com>
2023-04-17 15:46:58 +0200

    Add Guest Policies

--- app/Policies/GuestPolicy.php
+++ app/Policies/GuestPolicy.php
@@ -2,12 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Room;
+use App\Models\Guest;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Auth\Access\Response;
 
-class RoomPolicy {
+class GuestPolicy {
     use HandlesAuthorization;
 
     /**
@@ -16,27 +15,24 @@ class RoomPolicy {
      * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    static public function viewAny(User $user) {
+    public function viewAny(User $user) {
         $title = $user->role->title;
         return ($title === "owner" ||
             $title === "hotel manager" ||
-            $title === "head-housekeeping" ||
-            $title === "housekeeping" ||
             $title === "reception");
     }
+
     /**
      * Determine whether the user can view the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
+     * @param  \App\Models\Guest  $guest
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function view(User $user): bool {
+    public function view(User $user, Guest $guest) {
         $title = $user->role->title;
         return ($title === "owner" ||
             $title === "hotel manager" ||
-            $title === "head-housekeeping" ||
-            $title === "housekeeping" ||
             $title === "reception");
     }
 
@@ -49,56 +45,60 @@ class RoomPolicy {
     public function create(User $user) {
         $title = $user->role->title;
         return ($title === "owner" ||
-            $title === "hotel manager");
+            $title === "hotel manager" ||
+            $title === "reception");
     }
 
     /**
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
+     * @param  \App\Models\Guest  $guest
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function update(User $user) {
         $title = $user->role->title;
         return ($title === "owner" ||
-            $title === "hotel manager");
+            $title === "hotel manager" ||
+            $title === "reception");
     }
 
     /**
      * Determine whether the user can delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
+     * @param  \App\Models\Guest  $guest
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function delete(User $user) {
+    public function delete(User $user, Guest $guest) {
         $title = $user->role->title;
         return ($title === "owner" ||
-            $title === "hotel manager");
+            $title === "hotel manager" ||
+            $title === "reception");
     }
 
     /**
      * Determine whether the user can restore the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
+     * @param  \App\Models\Guest  $guest
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function restore(User $user) {
+    public function restore(User $user, Guest $guest) {
         $title = $user->role->title;
         return ($title === "owner" ||
-            $title === "hotel manager");
+            $title === "hotel manager" ||
+            $title === "reception");
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
+     * @param  \App\Models\Guest  $guest
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function forceDelete(User $user) {
+    public function forceDelete(User $user, Guest $guest) {
         //
     }
 }
