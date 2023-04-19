2365104b0bb94de88c305ae5d14b713d72e8c93f
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2023-04-19 12:20:13 +0200

    refurbish homepage for owner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,23 +9,7 @@
             <div class="flex-column align-center home-page-entry">
                 <h3>Reservations</h3>
 
-                <x-buttons.primary-button href="reservation/overview">Reservation overview</x-buttons.primary-button>
-
-                {{-- TODO: Make new route for date select --> room select --> route(reservation.create) --}}
-                <x-buttons.primary-button href="reservation/create">Make new reservation</x-buttons.primary-button>
-
-
-                <x-buttons.primary-button href="reservation/overview">View latest reservations</x-buttons.primary-button>
-            </div>
-
-            <div class="flex-column align-center home-page-entry">
-                <h3>Reception</h3>
-                <x-buttons.primary-button href="room/overview">Overview rooms</x-buttons.primary-button>
-
-                {{-- <div class="dates flex-center">
-
-                </div> --}}
-                {{-- TODO: Make new route for date select --> room overview --}}
+                <x-buttons.primary-button :href="route('reservation.overview')">Reservation overview</x-buttons.primary-button>
 
                 <div class="dateOfArrival">
                     <label for="dateOfArrival">Date of arrival</label>
@@ -42,14 +26,35 @@
                     @csrf
                     <x-buttons.primary-button class="flex-grow-1">View available rooms</x-buttons.primary-button>
                 </form>
+
+                {{-- TODO: Make new route for date select --> room select --> route(reservation.create) --}}
+                {{-- <x-buttons.primary-button href="reservation/create">Make new reservation</x-buttons.primary-button> --}}
+
+                {{-- 
+                <x-buttons.primary-button href="reservation/overview">View latest reservations</x-buttons.primary-button> --}}
             </div>
 
             <div class="flex-column align-center home-page-entry">
-                <h3>Housekeeping</h3>
+                <h3>Rooms</h3>
+                <x-buttons.primary-button :href="route('room.overview')">Room overview</x-buttons.primary-button>
+
+                <x-buttons.primary-button :href="route('room.create')">Add new room</x-buttons.primary-button>
+
+                {{-- <div class="dates flex-center">
+
+                </div> --}}
+                {{-- TODO: Make new route for date select --> room overview --}}
+
+            </div>
+
+            <div class="flex-column align-center home-page-entry">
+                <h3>Users</h3>
 
 
                 {{-- TODO: Make routes/overviews for housekeeping --}}
-                <x-buttons.primary-button href="room/overview">Overview rooms</x-buttons.primary-button>
+                <x-buttons.primary-button :href="route('user.overview')">User overview</x-buttons.primary-button>
+
+                <x-buttons.primary-button :href="route('user.register')">Register user</x-buttons.primary-button>
 
             </div>
 
