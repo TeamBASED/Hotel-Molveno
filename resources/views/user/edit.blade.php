5933d0cd371bf8e6d2118eb1c235d4671d7ef4e7
rogerfate <rogersnelders2001@gmail.com>
2023-04-21 11:06:47 +0200

    made layout like other edit pages

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -8,51 +8,48 @@
             <x-buttons.tertiary-button class="warning absolute-right" id="delete-button">Delete
             </x-buttons.tertiary-button>
             <x-delete-confirmation :removeId="$user->id" removalRoute='user.delete'></x-delete-confirmation>
-            <form method="POST" action="{{ route('user.update', ['user' => $user->id]) }}">
+            <form method="POST" class="grid-two-columns no-horizontal-gap" action="{{ route('user.update', ['user' => $user->id]) }}" >
                 @csrf
                 @method('PATCH')
-                <!-- Name -->
-                <!-- <div>
+
+                <div>
                     <x-input-label for="username" :value="__('Username:')" />
                     <x-text-input id="username" placeholder="Username" type="text" name="username" :value="old('username', $user->username)"
                         required autofocus autocomplete="username" />
                     <x-input-error :messages="$errors->get('username')" />
-                </div> -->
+                </div>
 
-                <!-- Email Address -->
-
-                <!-- <div>
-                    <x-input-label for="firstname" :value="__('First Name:')" />
-                    <x-text-input id="firstname" placeholder="Firstname" type="text" name="firstname"
-                        :value="old('firstname', $user->first_name)" required autocomplete="firstname" />
-                    <x-input-error :messages="$errors->get('firstname')" />
-                </div> -->
-
-                <!-- <div>
-                    <x-input-label for="lastname" :value="__('Last Name:')" />
-                    <x-text-input id="lastname" placeholder="Lastname" type="text" name="lastname" :value="old('lastname', $user->last_name)"
-                        required autocomplete="lastname" />
-                    <x-input-error :messages="$errors->get('lastname')" />
-                </div> -->
-
-                <!-- Password -->
-                <!-- <div>
+                <div>
                     <x-input-label for="password" :value="__('Password:')" />
                     <x-text-input id="password" placeholder="Password" type="password" name="password" />
 
                     <x-input-error :messages="$errors->get('password')" />
-                </div> -->
+                </div>
 
-                <!-- Confirm Password -->
-                <!-- <div>
-                    <x-input-label for="lastname" :value="__('Confirm Password:')" />
+                <div>
+                    <x-input-label for="firstname" :value="__('First Name:')" />
+                    <x-text-input id="firstname" placeholder="Firstname" type="text" name="firstname"
+                        :value="old('firstname', $user->first_name)" required autocomplete="firstname" />
+                    <x-input-error :messages="$errors->get('firstname')" />
+                </div>
+
+                <div>
+                    <x-input-label for="password_confirmation" :value="__('Confirm Password:')" />
                     <x-text-input id="password_confirmation" placeholder="Confirm Password" type="password"
                         name="password_confirmation" />
 
                     <x-input-error :messages="$errors->get('password_confirmation')" />
-                </div> -->
+                </div>
+                
+                <div>
+                    <x-input-label for="lastname" :value="__('Last Name:')" />
+                    <x-text-input id="lastname" placeholder="Lastname" type="text" name="lastname" :value="old('lastname', $user->last_name)"
+                        required autocomplete="lastname" />
+                    <x-input-error :messages="$errors->get('lastname')" />
+                </div>
+                
 
-                <!-- <div>
+                <div>
                     <x-input-label for="role" :value="__('Role:')" />
                     <select class="dropdown-select" name="role" required>
                         @foreach ($roles as $option)
@@ -62,16 +59,15 @@
                         @endforeach
                     </select>
                     <x-input-error :messages="$errors->get('role')" />
-                </div> -->
-
-                <!-- <div>
+                </div>
+                <div class="confirm-button-container">
                     {{-- <a href="{{ route('login') }}">
                         {{ __('Already registered?') }}
                     </a> --}}
                     <x-primary-button>
-                        {{ __('Confirm Changes') }}
+                        {{ __('save') }}
                     </x-primary-button>
-                </div> -->
+                </div>
             </form>
         </section>
     </main>
