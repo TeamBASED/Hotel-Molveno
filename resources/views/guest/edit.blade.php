6d179ecf2c72eef8ec220174e6a67ef0d84673dc
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-04-21 10:17:01 +0200

    update label styles on guest crud pages

--- resources/views/guest/edit.blade.php
+++ resources/views/guest/edit.blade.php
@@ -19,39 +19,39 @@
                 @method('PATCH')
                 <div class="flex-center">
                     <div class="flex-column half-width">
-                        <label for="first-name">First Name:</label>
+                        <label class="input-label" for="first-name">First Name:</label>
                         <input class="input-text" type="text" id="first-name" name="first_name" required
                             placeholder="Guest's first name" value="{{ old('first_name', $guest->first_name) }}">
                     </div>
 
                     <div class="flex-column half-width">
-                        <label for="last-name">Last Name:</label>
+                        <label class="input-label" for="last-name">Last Name:</label>
                         <input class="input-text" type="text" id="last-name" name="last_name" required
                             placeholder="Guest's last name" value="{{ old('last_name', $guest->last_name) }}">
                     </div>
                 </div>
 
                 <div class="flex-column">
-                    <label for="nationality">Nationality:</label>
+                    <label class="input-label" for="nationality">Nationality:</label>
                     <input class="input-text" type="text" id="nationality" name="nationality" required
                         placeholder="Guest's nationality" value="{{ old('nationality', $guest->nationality) }}">
                 </div>
 
                 <div class="flex-column">
-                    <label for="passport-number">Passport Number:</label>
+                    <label class="input-label" for="passport-number">Passport Number:</label>
                     <input class="input-text half-width" type="text" id="passport-number" name="passport_number"
                         required placeholder="Guest's passport number"
                         value="{{ old('passport_number', $guest->passport_number) }}">
                 </div>
 
                 <div class="flex-column">
-                    <label for="date-of-birth">Date of Birth:</label>
+                    <label class="input-label" for="date-of-birth">Date of Birth:</label>
                     <input class="input-text half-width" type="date" id="date-of-birth" name="date_of_birth" required
                         placeholder="Guest's date of birth" value="{{ old('date_of_birth', $guest->date_of_birth) }}">
                 </div>
 
                 <div class="flex-column">
-                    <label for="date-of-birth">Passport checked?</label>
+                    <label class="input-label" for="date-of-birth">Passport checked?</label>
                     <input class="input-text half-width" type="checkbox" id="passport-checked" name="passport_checked"
                         value="1" {{ old('passport_checked', $guest->passport_checked == 1 ? 'checked' : '') }}>
                 </div>
