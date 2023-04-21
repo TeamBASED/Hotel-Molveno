05759d60a4e482e0bc71197b48030f436098dc1f
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-04-21 10:13:42 +0200

    update label styling on room crud

--- resources/views/room/edit.blade.php
+++ resources/views/room/edit.blade.php
@@ -29,22 +29,22 @@
                 @csrf
                 @method('PATCH')
                 <div class="flex-column padding-block-1rem">
-                    <label for="room-number">Room number:</label>
+                    <label class="input-label" for="room-number">Room number:</label>
                     <input id="room-number" type="text" class="input-text" required placeholder="Room number"
                         name="number" value="{{ old('number', $room->room_number) }}">
-                    <label for="capacity">Capacity:</label>
+                    <label class="input-label" for="capacity">Capacity:</label>
                     <input id="capacity" type="text" class="input-text" required placeholder="Capacity"
                         name="capacity" value="{{ old('capacity', $room->capacity) }}">
-                    <label for="price-per-night">Price per night:</label>
+                    <label class="input-label" for="price-per-night">Price per night:</label>
                     <input id="price-per-night" type="text" class="input-text" required placeholder="Price per night"
                         name="price" value="{{ old('price', $room->base_price_per_night) }}">
                     <div class="inline-input">
-                        <label for="single-bed-configuration">Single beds:</label>
+                        <label class="input-label" for="single-bed-configuration">Single beds:</label>
                         <input id="single-bed-configuration" type="number" class="input-text" required
                             name="singleBeds" value="{{ old('configuration', $singleBeds) }}">
                     </div>
                     <div class="inline-input">
-                        <label for="double-bed-configuration">Double beds</label>
+                        <label class="input-label" for="double-bed-configuration">Double beds</label>
                         <input id="double-bed-configuration" type="number" class="input-text" required
                             name="doubleBeds" value="{{ old('configuration', $doubleBeds) }}">
                     </div>
@@ -52,18 +52,18 @@
                 </div>
                 <div class="flex-column padding-block-1rem">
                     <div class="flex-space-between flex-aling-center">
-                        <label for="baby-bed">Baby bed possible:</label>
+                        <label class="input-label" for="baby-bed">Baby bed possible:</label>
                         <input id="baby-bed" type="checkbox" class="input-text" name="babybed" value="1"
                             {{ $room->baby_bed_possible == 1 ? 'checked' : '' }}>
                     </div>
-                    <label for="room-edit-description">Description:</label>
+                    <label class="input-label" for="room-edit-description">Description:</label>
                     <textarea class="input-text flex-grow-1" id="room-edit-description" placeholder="Room description" name="description">{{ old('description', $room->description) }}</textarea>
                 </div>
 
 
 
                 <div class="flex-column padding-block-1rem">
-                    <label for="room-type">Room type:</label>
+                    <label class="input-label" for="room-type">Room type:</label>
                     <select id="room-type" class="dropdown-select" name="type" required>
                         @foreach ($roomTypes as $option)
                             <option class="filter-field-option" value="{{ $option->id }}"
@@ -71,7 +71,7 @@
                             </option>
                         @endforeach
                     </select>
-                    <label for="room-view">Room view:</label>
+                    <label class="input-label" for="room-view">Room view:</label>
                     <select id="room-view" class="dropdown-select" name="view" required>
                         @foreach ($roomViews as $option)
                             <option class="filter-field-option" value="{{ $option->id }}"
