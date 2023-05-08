7c4eccab2cbe8b26f4a674818613dda9602f6185
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-05-08 14:41:33 +0200

    make invoice factory

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -5,15 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Invoice extends Model
-{
+class Invoice extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'payment_method',
+        'value_added_tax',
+        'cost_adjustment',
+        'final_amount'
+    ];
 
     public function getInvoiceById($id) {
         return Invoice::where('id', $id)->first();
     }
 
-    public function reservation() { 
+    public function reservation() {
         return $this->belongsTo(Reservation::class);
     }
 }
--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -7,17 +7,19 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
  */
-class InvoiceFactory extends Factory
-{
+class InvoiceFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition()
-    {
+    public function definition() {
         return [
-            //
+            'payment_method_id' => fake()->numberBetween(1, 3),
+            'value_added_tax' => fake()->numberBetween(10, 30),
+            'cost_adjustment' => fake()->numberBetween(0, 100),
+            'final_amount' => fake()->numberBetween(600, 2000),
+            'comment' => fake()->sentence(),
         ];
     }
 }
