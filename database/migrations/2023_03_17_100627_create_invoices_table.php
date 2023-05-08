69fc5135115c0ad0f48b65f994a6649ce7ef1116
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-05-08 14:25:52 +0200

    make invoices migration

--- database/migrations/2023_03_17_100627_create_invoices_table.php
+++ database/migrations/2023_03_17_100627_create_invoices_table.php
@@ -4,18 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("payment_method");
+            $table->float("value_added_tax");
+            $table->float("cost_adjustment");
+            $table->float("final_amount");
         });
     }
 
@@ -24,8 +26,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('invoices');
     }
 };
