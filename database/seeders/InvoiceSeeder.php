b1144768a3dac7acdf353e598d356c3eb9e4d96e
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-05-08 15:48:44 +0200

    seed invoices

--- database/seeders/InvoiceSeeder.php
+++ database/seeders/InvoiceSeeder.php
@@ -2,18 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Invoice;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class InvoiceSeeder extends Seeder
-{
+class InvoiceSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+        Invoice::factory()->count(10)->create();
     }
 }
