212cbd33f3d9ad9964a04ef9284632a2eaa85237
rogerfate <rogersnelders2001@gmail.com>
2023-05-10 10:37:21 +0200

    Correct labels

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -4,10 +4,12 @@
             <div id="heading-container">
                 <h2>Register User</h2>
             </div>
+            <!-- <x-buttons.tertiary-button class="warning absolute-right" id="delete-button">Delete
+            </x-buttons.tertiary-button>
+            <x-delete-confirmation :removeId="$user->id" :removalRoute="route('user.delete', $user->id)"></x-delete-confirmation> -->
             <form method="POST" action="{{ route('user.store') }}">
                 @csrf
 
-                <!-- Name -->
                 <div>
                     <x-input-label for="username" :value="__('Username:')" />
                     <x-text-input id="username" placeholder="Username" type="text" name="username" :value="old('username')"
@@ -15,34 +17,30 @@
                     <x-input-error :messages="$errors->get('username')" />
                 </div>
 
-                <!-- Email Address -->
-
                 <div>
-                    <x-input-label for="username" :value="__('First Name:')" />
+                    <x-input-label for="firstname" :value="__('First Name:')" />
                     <x-text-input id="firstname" placeholder="Firstname" type="text" name="firstname"
                         :value="old('firstname')" required autocomplete="firstname" />
                     <x-input-error :messages="$errors->get('firstname')" />
                 </div>
 
                 <div>
-                    <x-input-label for="username" :value="__('Last Name:')" />
+                    <x-input-label for="lastname" :value="__('Last Name:')" />
                     <x-text-input id="lastname" placeholder="Lastname" type="text" name="lastname" :value="old('lastname')"
                         required autocomplete="lastname" />
                     <x-input-error :messages="$errors->get('lastname')" />
                 </div>
 
-                <!-- Password -->
                 <div>
-                    <x-input-label for="username" :value="__('Password:')" />
+                    <x-input-label for="password" :value="__('Password:')" />
                     <x-text-input id="password" placeholder="Password" type="password" name="password" required
                         autocomplete="new-password" />
 
                     <x-input-error :messages="$errors->get('password')" />
                 </div>
 
-                <!-- Confirm Password -->
                 <div>
-                    <x-input-label for="username" :value="__('Confirm Password:')" />
+                    <x-input-label for="password_confirmation" :value="__('Confirm Password:')" />
                     <x-text-input id="password_confirmation" placeholder="Confirm Password" type="password"
                         name="password_confirmation" required autocomplete="new-password" />
 
@@ -61,13 +59,13 @@
                     <x-input-error :messages="$errors->get('role')" />
                 </div>
 
-                <div>
-                    {{-- <a href="{{ route('login') }}">
-                        {{ __('Already registered?') }}
-                    </a> --}}
-                    <x-primary-button>
-                        {{ __('Register') }}
-                    </x-primary-button>
+                <div class="save-button-container">
+                    <x-buttons.secondary-button :href="route('user.overview')">
+                        {{ __('cancel') }}
+                    </x-buttons.secondary-button>
+                    <x-buttons.primary-button form="user-edit-form">
+                        {{ __('save') }}
+                    </x-buttons.primary-button>
                 </div>
             </form>
         </section>
