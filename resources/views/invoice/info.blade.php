3358829ad3860224e60a281714867a345130828c
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-05-12 11:19:01 +0200

    add info file

---
+++ resources/views/invoice/info.blade.php
@@ -0,0 +1,20 @@
+<x-layout.base>
+    <main id="invoice-info" class="main-content">
+
+        <article class="details-section">
+            <h2>Invoice info</h2>
+
+            <div>
+                <p>Label</p>
+                <p>Value</p>
+            </div>
+
+
+        </article>
+
+        <div>
+            buttons
+        </div>
+
+    </main>
+</x-layout.base>
