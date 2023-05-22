ffcf788aa6b2a1379b1dc035c0ebcf3ffb58a855
Tom Damen <twjdamen@gmail.com>
2023-05-22 09:15:14 +0200

    remove make reservation button if room not avail

--- resources/views/components/room/card.blade.php
+++ resources/views/components/room/card.blade.php
@@ -21,13 +21,18 @@
     @endif
 
     {{-- Temporary solution for routing to reservation create --}}
+
+
     @can('create', App\Models\Reservation::class)
-        <form action="{{ route('reservation.contact', ['id' => $room->id]) }}" method="GET">
+        <form action="{{ route('reservation.contact', ['id' => $room->id]) }}" method="GET"
+            @if (!isset($room->availableUntil)) style="visibility: hidden" @endif>
             @csrf
             <input type="hidden" name="roomId" value="{{ $room->id }}">
-            <x-buttons.primary-button>Make reservation</x-buttons.primary-button>
+            <x-buttons.primary-button>Make reservation
+            </x-buttons.primary-button>
         </form>
     @endcan
+
     @can('update', App\Models\CleaningStatus::class)
         <x-buttons.secondary-button class="cleaning-menu-button">Change cleaning status
         </x-buttons.secondary-button>
